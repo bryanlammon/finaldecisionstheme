4664ab5d7e4e2dae48c10ece1843b439e6539114
Bryan Lammon <>
2019-06-28 11:46:33 -0400

    Added related-posts block

--- single.php
+++ single.php
@@ -36,6 +36,31 @@
 
 				<section class="related-cards">
 					<h4>Related Posts</h4>
+					<dl>
+						<?php
+							$tags = wp_get_post_tags( $post -> ID );
+							if ($tags) {
+								$first_tag = $tags[0] -> term_id;
+								$args=array(
+									'tag__in' => array($first_tag),
+									'post__not_in' => array($post->ID),
+									'posts_per_page'=>5,
+									'caller_get_posts'=>1
+								);
+
+								$my_query = new WP_Query($args);
+								if( $my_query->have_posts() ) {
+									while ($my_query->have_posts()) : $my_query->the_post(); ?>
+									<dt><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></dt>
+									<di><?php the_subtitle(); ?></di>
+
+							<?php
+								endwhile;
+								}
+								wp_reset_query();
+								}
+						?>
+					</dl>
 				</section>
 			</section>
 

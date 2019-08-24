3c586647cf9e087133124b256ad848fcb5ec1311
Bryan Lammon <bryanlammon@Bryans-MacBook-Pro.local>
2019-08-24 18:54:58 -0400

    Revised search template

--- search.php
+++ search.php
@@ -1,31 +1,40 @@
-<!-- OLD -->
-
 <?php
-/*
- *
- * The template for displaying search results.
- *
- */
+
+//
+// Final Decisions rev 3
+// Search template file
+// Last update: Rev 3a1 on August 24, 2019
+//
+
+get_header();
 ?>
 
-<?php get_header(); ?>
-
 		<main>
-			<section id="searched">
-				<div class="blog-about">
-					<h1 class="search-title">
-						<?php echo $wp_query->found_posts; ?> <?php _e( 'Search Results Found For', 'locale' ); ?>: &ldquo;<?php the_search_query(); ?>&rdquo;
-					</h1>
+
+			<?php get_template_part( 'template-parts/random-background' ); ?>
+
+			<div class='row row--bottom margin'>
+				<div class='container title-container title-container--narrow'>
+					<h1 class='title'><?php echo $wp_query->found_posts; ?> <?php _e( 'Search Results Found For', 'locale' ); ?>: &ldquo;<?php the_search_query(); ?>&rdquo;</h1>
 				</div>
-			</section><!-- /#searched -->
+			</div>
 
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content/content-excerpts', get_post_format() );
+				endwhile;
+			endif;
+			?>
 
-
-			<section class="posts-container">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); get_template_part( 'template-parts/content/content', get_post_format() ); endwhile; endif; ?>
-			</section>
+			<div class='row row--bottom-margin'>
+				<hr>
+				<div class='container container--wrap container--justify-center'>
+					<?php posts_nav_link(' ','Newer Posts','Older Posts'); ?>
+				</div>
+			</div>
 
 		</main>
-
 
 <?php get_footer(); ?>

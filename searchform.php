b9c96540ad30feb30cdf248aada3d8bc28b14e87
Bryan Lammon <bryanlammon@Bryans-MacBook-Pro.local>
2019-08-24 19:11:56 -0400

    Delete searchform.php
    Deleted the old searchform.php, as the code is now in the header

--- searchform.php
+++
@@ -1,9 +0,0 @@
-<!-- OLD -->
-
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div>
-		<label class="screen-reader-text" for="s" hidden>Search for:</label>
-		<input type="text" placeholder="Search Posts" value="" name="s" id="s">
-		<input type="submit" id="searchsubmit" value="Search" hidden>
-	</div>
-</form>

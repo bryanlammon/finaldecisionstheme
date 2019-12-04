0bffd7c4f43f42a84064d1b0d3f1913359ef65c9
Bryan Lammon <bryanlammon@Bryans-MacBook-Pro.local>
2019-12-03 19:20:14 -0500

    Removed cache-clear code
    Removed cache-clear code from header.php. Site should start caching again.

--- header.php
+++ header.php
@@ -17,13 +17,6 @@
 
 		<meta name="description" content=<?php echo get_bloginfo( 'description' ); ?>>
 
-		<!-- Clear cache -->
-		<meta http-equiv="cache-control" content="max-age=0" />
-		<meta http-equiv="cache-control" content="no-cache" />
-		<meta http-equiv="expires" content="0" />
-		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
-		<meta http-equiv="pragma" content="no-cache" />
-
 		<?php wp_head(); ?>
 
 	</head>

e77f30feb3179767da24d542abc1a2f702bee5ee
bryanlammon <50211594+bryanlammon@users.noreply.github.com>
2023-04-16 10:37:36 -0500

    Updated dark color scheme

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function my_queue()
 {
 
     // Google Fonts
-    wp_enqueue_style('google_fonts', 'https://fonts.googleapis.com/css?family=Cormorant+Garamond:600,600i,700,700i|Raleway:700');
+    wp_enqueue_style('google_fonts', 'https://fonts.googleapis.com/css?family=Cormorant+Garamond:600,600i,700,700i|Raleway:400,700');
     // Normalize CSS
     wp_enqueue_style('normalizecss', get_template_directory_uri() . '/css/normalize.css');
     // Font Awesome
